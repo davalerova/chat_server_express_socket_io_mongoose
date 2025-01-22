7e5ad559056123e4b22a4f6a8a2e91145c52eeb6
David Valero <dvdovni@gmail.com>
2025-01-22 18:35:40 -0500

    Conectar Node con Mongo Atlas

--- database/config.js
+++ database/config.js
@@ -3,8 +3,9 @@ require('dotenv').config();
 
 const dbConnection = async () => {
     try {
-        
         console.log('Conectando con MongoDB...');
+        await mongoose.connect(process.env.DB_CNN);
+        console.log('Conexión con MongoDB establecida');
     } catch (error) {
         console.log('Error al conectar con MongoDB:', error);
         process.exit(1);
