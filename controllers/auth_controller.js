51ec7ce02926624d27c5b62703716e59f83c9453
David Valero <dvdovni@gmail.com>
2025-01-22 19:43:04 -0500

    Middleware personalizado - ValidarCampos

--- controllers/auth_controller.js
+++ controllers/auth_controller.js
@@ -2,11 +2,6 @@ const { response } = require("express");
 const { validationResult } = require("express-validator");
 
 const crearUsuario = (req, res = response) => {
-
-    const errores = validationResult(req);
-    if (!errores.isEmpty()) {
-        return res.status(400).json({ ok: false, errores: errores.mapped() });
-    }
     res.status(200).json({ message: 'Bienvenido a nuestro chat server' });
 };
 
--- routes/auth.js
+++ routes/auth.js
@@ -7,11 +7,18 @@ const { Router } = require('express');
 const { check } = require('express-validator');
 
 const { crearUsuario } = require('../controllers/auth_controller');
+const { validarCampos } = require('../middlewares/validar-campos');
 
 const router = Router();
 
 router.post('/new', [
-    check('nombre', 'El nombre es obligatorio').not().isEmpty(),
+    check('nombre', 'El nombre es obligatorio').trim().not().isEmpty(),
+    check('nombre', 'El nombre debe tener entre 3 y 50 caracteres').trim().isLength({ min: 3, max: 50 }),
+    check('email', 'El email es obligatorio').trim().not().isEmpty(),
+    check('email', 'El email es inválido').trim().isEmail(),
+    check('password', 'La contraseña es obligatoria').trim().not().isEmpty(),
+    check('password', 'La contraseña debe tener por lo menos 8 caracteres').trim().isLength({ min: 8 }),
+    validarCampos
 ], crearUsuario);
 
 module.exports = router;
